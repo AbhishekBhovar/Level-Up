6ca65817d28c63cb064e2faa5b4e2ecc3494c1c1
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-07 00:50:05 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='level-up-fitness-v79-bench-mastery-ww-20260907';
+const CACHE='level-up-fitness-v80-five-fixes-rebuild-20260907';
 const PREFIX='level-up-fitness-';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./assets/level_up_logo_final_v78.png','./assets/23_wonder_woman_JLU_v79.jpeg'])))});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./assets/level_up_logo_final_v78.png','./assets/23_wonder_woman_JLU_v80.jpeg']))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
